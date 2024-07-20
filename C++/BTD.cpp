c41c1c824036035598c6e83adb4c1ea9bbbe0eee
Waleed-Ahmed-05 <LP.Gangster05@gmail.com>
2024-07-21 00:04:56 +0500

    Added file handling and done all necessary modifications.

--- C++/BTD.cpp
+++ C++/BTD.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <cctype>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 
@@ -94,6 +95,45 @@ bool Binary_Validator(int Number)
     
     return Valid;
 }
+void History(int Status, string Number, string Result)
+{
+    fstream History;
+
+    History.open("History.txt", ios::app);
+
+    if(Status == 0){History << Number << " Bin - " << Result << " Dec" << endl;}
+    else if(Status == 1){History << Number << " Dec - " << Result << " Bin" << endl;}
+    else if(Status == 2)
+	{
+		int Count_01 = 0,Count_02 = 0;
+
+		History.close();
+
+		History.open("History.txt",ios::in);
+
+		system("cls");
+		
+		while(!History.eof())
+		{
+			Count_01++;
+			getline(History, Statement);
+		}
+
+		History.close();
+
+		History.open("History.txt",ios::in);
+
+		while(!History.eof())
+		{
+			Count_02++;
+			getline(History, Statement);
+			cout << Count_02 << ". " << Statement << endl << endl;
+			if(Count_01 - 1 == Count_02){break;}
+		}
+	}
+
+    History.close();
+}
 void BinToDec()
 {
     int Number_01,Number_02,Count_01;
@@ -120,6 +160,8 @@ void BinToDec()
 
             }
 
+            History(0, to_string(Number_02), to_string(Result));
+
             system("cls");
             
             cout << "Binary number " << Number_02 << " in decimal is " << Result << "." << endl << endl << "Press enter key to continue: ";
@@ -140,18 +182,55 @@ void DecToBin()
 
     Statement = Divisor(BTD);
 
+    History(1, to_string(BTD), Statement);
+
     system("cls");
 
     cout << "Decimal number " << BTD << " in binary is " << Statement << "." << endl << endl << "Press enter key to continue: ";
 
     cin.sync();
     getline(cin, Input);
-    
 }
-void History(){}
+void Animation(int Status)
+{
+    if(Status == 0)
+    {
+        Statement = "Binary or Decimal converter is being Initialized ";
+
+        for(int i = 0;i < sizeof(Statement) + 25;i++)
+        {
+            Sleep(0);
+            cout << Statement[i];
+        }
+    }
+    else if(Status == 1)
+    {
+        Statement = "Binary or Decimal converter is being terminated ";
+
+        for(int i = 0;i < sizeof(Statement) + 24;i++)
+        {
+            Sleep(0);
+            cout << Statement[i];
+        }
+    }
+	else if(Status == 2)
+	{
+		Statement = ". . . . . . . . . . . . . . ";
+
+		for(int i = 0;i < sizeof(Statement);i++)
+        {
+            Sleep(0);
+            cout << Statement[i];
+        }
+	}
+}
 
 int main()
 {
+    system("cls");
+
+    Animation(0);Animation(2);
+
     while(true)
     {
         system("cls");
@@ -161,8 +240,22 @@ int main()
 
         if(Statement == "1"){BinToDec();}
         else if(Statement == "2"){DecToBin();}
-        else if(Statement == "3"){History();}
+        else if(Statement == "3")
+        {
+            History(2, "", "");
+
+            cout << "Press enter key to continue: ";
+
+            cin.sync();
+            getline(cin, Input);
+        }
         else if(Statement == "4"){break;}
         else{system("cls");cout << "! ! ! Invalid choice ! ! !";Sleep(1);}
     }
+
+    system("cls");
+
+    Animation(1);Animation(2);
+
+    system("cls");
 }
